b1a4e2d09b1b864cefc850868174dfa52735af92
manukumarcode23 <onetaplooters@gmail.com>
2025-01-12 15:25:40 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7115941391:AAE_MvX8E0cxYvWEy774VeMi-LmDQs3JZcs")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@CodeBreakerX9_bot")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6290531180")
+
+
+
+
+
+
+
+  
